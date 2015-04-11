557f2fd07a16ffecee4b4beaba9f7c3cc6724f46
Gustavo <gustavobenn@hotmail.com>
2015-04-11 20:34:49 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ else:
                    include_dirs=[numpy.get_include(), '/usr/local/include', 'tsne/bh_sne_src/'],
                    library_dirs=['/usr/local/lib'],
                    extra_compile_args=['-msse2', '-O3', '-fPIC', '-w'],
-                   extra_link_args=['-lcblas'],
+                   extra_link_args=['-lopenblas'],
                    language='c++')]
 
 setup(
